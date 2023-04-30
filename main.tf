624816fe06794d344d5f341c0a007d513c3c0f36
Vivi Tian <vivi.tian930@gmail.com>
2023-04-30 17:49:31 +1200

    feat: App configuration with key values and feature flags

---
+++ main.tf
@@ -0,0 +1,46 @@
+resource "azurerm_app_configuration" "appconf" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+}
+
+resource "azurerm_role_assignment" "appconf_dataowner" {
+  scope                = azurerm_app_configuration.appconf.id
+  role_definition_name = "App Configuration Data Owner"
+  principal_id         = data.azurerm_client_config.current.object_id
+}
+
+resource "azurerm_app_configuration_key" "key_values" {
+  for_each               = var.key_values
+  configuration_store_id = azurerm_app_configuration.appconf.id
+  key                    = each.value.key
+  label                  = each.value.label
+  value                  = each.value.value
+
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_key" "kv_reference" {
+  for_each               = var.key_values_kv_reference
+  configuration_store_id = azurerm_app_configuration.appconf.id
+  key                    = each.key.key
+  type                   = "vault"
+  label                  = each.key.label
+  vault_key_reference    = each.key.vault_key_reference
+
+  depends_on = [
+    azurerm_role_assignment.appconf_dataowner
+  ]
+}
+
+resource "azurerm_app_configuration_feature" "feature_flags" {
+  for_each               = var.feature_flags
+  configuration_store_id = azurerm_app_configuration.appconf.id
+  description            = each.value.description
+  name                   = each.value.name
+  label                  = each.value.label
+  enabled                = each.value.enabled
+}
+
